f1a580c78379b4f69e2405957a120976b5e8d2ee
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-22 23:24:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
-def game_hash 
+def game_hash
   gamehash = {
     :home => {
       :team_name => "Brooklyn Nets",
@@ -121,7 +121,7 @@ def game_hash
             :assists => 12,
             :steals => 7,
             :blocks => 5,
-            :slam_dunks => 12 
+            :slam_dunks => 12
           }
         ]
       }
